ef259208cd8f1e282f6460fedca20fcf3a3b9ec3
DMAKCODESERIES <daniyalabbasi0349@gmail.com>
2025-01-12 20:00:11 +0500

    mske some changes

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -4,15 +4,15 @@ import {assets} from "../../assets/assets";
 
 const Navbar = () => {
   return (
-    <div className="bg-blue-100 display flex justify-between text-black font-bold p-4 w-full h-10">
+    <div className="bg-blue-100 display flex  text-black font-bold p-4 w-full h-10 justify-between items-center ">
       <img src={assets.logo} alt="logo" className="navbar__logo" />
-      <ul className="display flex justify-between  gap-3 ">
+      <ul className="display flex justify-between  gap-5 ">
         <li className="navbar__item">Home</li>
         <li className="navbar__item">menu</li>
         <li className="navbar__item">mobile-app</li>
         <li className="navbar__item">Contact</li>
         </ul>
-        <div>
+        <div className="flex  gap-5 justify-between ">
             <img src={assets.search_icon} alt="search_icon" className="navbar__search" />
             <div>
                 <img src={assets.basket_icon} alt="basket_icon" className="navbar__basket" />
